b3c3d731e5b8f3ee70008f51d6d5fb3a02be5cc2
John Ottenlips <johno@aloompa.com>
2019-12-21 18:17:44 -0600

    add script for getting theme for season

---
+++ get_theme_season.sh
@@ -0,0 +1,39 @@
+month=$(date +"%m")
+case $month in
+"1")
+    echo "winter"
+    ;;
+"2")
+    echo "winter"
+    ;;
+"3")
+    echo "spring"
+    ;;
+"4")
+    echo "spring"
+    ;;
+"5")
+    echo "spring"
+    ;;
+"6")
+    echo "seashell"
+    ;;
+"7")
+    echo "seashell"
+    ;;
+"8")
+    echo "seashell"
+    ;;
+"9")
+    echo "fall"
+    ;;
+"10")
+    echo "fall"
+    ;;
+"11")
+    echo "fall"
+    ;;
+"12")
+    echo "holiday"
+    ;;
+esac
